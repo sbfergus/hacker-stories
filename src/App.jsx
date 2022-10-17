5a903732f211fac274763926c6ea4fce02a2a7fd
Scott <sbferguson17@gmail.com>
2022-10-17 11:40:03 -0600

    mimic api call

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,40 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Wilke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clarke",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const getAsyncStories = () => {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
+  );
+};
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
-  const [stories, setStories] = useState([
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Wilke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clarke",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ]);
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    getAsyncStories().then((result) => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
